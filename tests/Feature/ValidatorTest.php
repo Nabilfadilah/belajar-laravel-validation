a51ebe38fcce22a15aab1cd3efd8f529bdb07b48
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-09-04 10:32:04 +0700

    Rule Class dan menjalankan unit test

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\In;
+use Illuminate\Validation\Rules\Password;
 use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
@@ -285,23 +287,24 @@ class ValidatorTest extends TestCase
         Log::info($message->toJson(JSON_PRETTY_PRINT));
     }
 
-    // public function testValidatorRuleClasses()
-    // {
-    //     $data = [
-    //         "username" => "Eko",
-    //         "password" => "eko@pzn123.com"
-    //     ];
+    // rule class
+    public function testValidatorRuleClasses()
+    {
+        $data = [
+            "username" => "Abil",
+            "password" => "abil@pzn123.com"
+        ];
 
-    //     $rules = [
-    //         "username" => ["required", new In(["Eko", "Budi", "Joko"])],
-    //         "password" => ["required", Password::min(6)->letters()->numbers()->symbols()]
-    //     ];
+        $rules = [
+            "username" => ["required", new In(["Abil", "Bula", "Klop"])], // hanya username ini yg boleh input // pake class dari Rule package
+            "password" => ["required", Password::min(6)->letters()->numbers()->symbols()] // pake class dari Rule package
+        ];
 
-    //     $validator = Validator::make($data, $rules);
-    //     self::assertNotNull($validator);
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
 
-    //     self::assertTrue($validator->passes());
-    // }
+        self::assertTrue($validator->passes());
+    }
 
     // public function testNestedArray()
     // {
