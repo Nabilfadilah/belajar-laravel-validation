bec6c61a09261c8798fb991d0b6ec8cd2edaa6bc
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-09-04 11:09:37 +0700

    Error Directive update form input

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -18,12 +18,26 @@
             </ul>
         @endif
 
-        <form action="/form" method="POST">
+        {{-- error directive --}}
+        <form action="/form" method="post">
+            @csrf
+            <label>Username : @error('username')
+                    {{ $message }}
+                @enderror
+                <input type="text" name="username" value="{{ old('username') }}"></label> <br>
+            <label>Password : @error('password')
+                    {{ $message }}
+                @enderror
+                <input type="password" name="password" value="{{ old('password') }}"></label><br>
+            <input type="submit" value="Login">
+        </form>
+
+        {{-- <form action="/form" method="POST">
             @csrf
             <label>Username : <input type="text" name="username"></label>
             <label>Password : <input type="password" name="password"></label>
             <input type="submit" value="Login">
-        </form>
+        </form> --}}
     </div>
 </body>
 
