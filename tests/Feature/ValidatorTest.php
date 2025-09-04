52b35a708fe83fb85ca64e54d8bd436f6a3fea95
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-09-04 08:59:52 +0700

    Error Message dan menjalankan unit test

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
@@ -32,7 +33,7 @@ class ValidatorTest extends TestCase
         self::assertFalse($validator->fails()); // fails(), akan mengembalikan true jika gagal, false jika sukses
     }
 
-    public function testInValidator(): void
+    public function testInvalidValidator(): void
     {
         // variable data, yg isinya array  
         $data = [
@@ -52,5 +53,277 @@ class ValidatorTest extends TestCase
         // menjalankan validasi
         self::assertFalse($validator->passes()); // passes(), akan mengembalikan true jika sukses, false jika gagal
         self::assertTrue($validator->fails()); // fails(), akan mengembalikan true jika gagal, false jika sukses
+
+        // mendapatkan detail dari error getMessageBag()
+        $message = $validator->getMessageBag();
+        // $message->get('username'); // keys() ->get(), mau lihat errornya apak aja, misalnya 'username' atau 'name', dll
+
+        // translate jadi Json
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
     }
+
+    // public function testValidatorValidationException()
+    // {
+    //     $data = [
+    //         "username" => "",
+    //         "password" => ""
+    //     ];
+
+    //     $rules = [
+    //         "username" => "required",
+    //         "password" => "required"
+    //     ];
+
+    //     $validator = Validator::make($data, $rules);
+    //     self::assertNotNull($validator);
+
+    //     try {
+    //         $validator->validate();
+    //         self::fail("ValidationException not thrown");
+    //     }catch (ValidationException $exception){
+    //         self::assertNotNull($exception->validator);
+    //         $message = $exception->validator->errors();
+    //         Log::error($message->toJson(JSON_PRETTY_PRINT));
+    //     }
+    // }
+
+    // public function testValidatorMultipleRules()
+    // {
+    //     App::setLocale("id");
+
+    //     $data = [
+    //         "username" => "eko",
+    //         "password" => "eko"
+    //     ];
+
+    //     $rules = [
+    //         "username" => "required|email|max:100",
+    //         "password" => ["required", "min:6", "max:20"]
+    //     ];
+
+    //     $validator = Validator::make($data, $rules);
+    //     self::assertNotNull($validator);
+
+    //     self::assertFalse($validator->passes());
+    //     self::assertTrue($validator->fails());
+
+    //     $message = $validator->getMessageBag();
+
+    //     Log::info($message->toJson(JSON_PRETTY_PRINT));
+    // }
+
+    // public function testValidatorValidData()
+    // {
+    //     $data = [
+    //         "username" => "admin@pzn.com",
+    //         "password" => "rahasia",
+    //         "admin" => true,
+    //         "others" => "xxx"
+    //     ];
+
+    //     $rules = [
+    //         "username" => "required|email|max:100",
+    //         "password" => "required|min:6|max:20"
+    //     ];
+
+    //     $validator = Validator::make($data, $rules);
+    //     self::assertNotNull($validator);
+
+    //     try {
+    //         $valid = $validator->validate();
+    //         Log::info(json_encode($valid, JSON_PRETTY_PRINT));
+    //     }catch (ValidationException $exception){
+    //         self::assertNotNull($exception->validator);
+    //         $message = $exception->validator->errors();
+    //         Log::error($message->toJson(JSON_PRETTY_PRINT));
+    //     }
+    // }
+
+    // public function testValidatorInlineMessage()
+    // {
+    //     $data = [
+    //         "username" => "eko",
+    //         "password" => "eko"
+    //     ];
+
+    //     $rules = [
+    //         "username" => "required|email|max:100",
+    //         "password" => ["required", "min:6", "max:20"]
+    //     ];
+
+    //     $messages = [
+    //         "required" => ":attribute harus diisi",
+    //         "email" => ":attribute harus berupa email",
+    //         "min" => ":attribute minimal :min karakter",
+    //         "max" => ":attribute maksimal :max karakter",
+    //     ];
+
+    //     $validator = Validator::make($data, $rules, $messages);
+    //     self::assertNotNull($validator);
+
+    //     self::assertFalse($validator->passes());
+    //     self::assertTrue($validator->fails());
+
+    //     $message = $validator->getMessageBag();
+
+    //     Log::info($message->toJson(JSON_PRETTY_PRINT));
+    // }
+
+    // public function testValidatorAdditionalValidation()
+    // {
+    //     $data = [
+    //         "username" => "eko@pzn.com",
+    //         "password" => "eko@pzn.com"
+    //     ];
+
+    //     $rules = [
+    //         "username" => "required|email|max:100",
+    //         "password" => ["required", "min:6", "max:20"]
+    //     ];
+
+    //     $validator = Validator::make($data, $rules);
+    //     $validator->after(function (\Illuminate\Validation\Validator $validator){
+    //         $data = $validator->getData();
+    //         if($data['username'] == $data['password']){
+    //             $validator->errors()->add("password", "Password tidak boleh sama dengan username");
+    //         }
+    //     });
+    //     self::assertNotNull($validator);
+
+    //     self::assertFalse($validator->passes());
+    //     self::assertTrue($validator->fails());
+
+    //     $message = $validator->getMessageBag();
+
+    //     Log::info($message->toJson(JSON_PRETTY_PRINT));
+    // }
+
+    // public function testValidatorCustomRule()
+    // {
+    //     $data = [
+    //         "username" => "eko@pzn.com",
+    //         "password" => "eko@pzn.com"
+    //     ];
+
+    //     $rules = [
+    //         "username" => ["required", "email", "max:100", new Uppercase()],
+    //         "password" => ["required", "min:6", "max:20", new RegistrationRule()]
+    //     ];
+
+    //     $validator = Validator::make($data, $rules);
+    //     self::assertNotNull($validator);
+
+    //     self::assertFalse($validator->passes());
+    //     self::assertTrue($validator->fails());
+
+    //     $message = $validator->getMessageBag();
+
+    //     Log::info($message->toJson(JSON_PRETTY_PRINT));
+    // }
+
+    // public function testValidatorCustomFunctionRule()
+    // {
+    //     $data = [
+    //         "username" => "eko@pzn.com",
+    //         "password" => "eko@pzn.com"
+    //     ];
+
+    //     $rules = [
+    //         "username" => ["required", "email", "max:100", function(string $attribute, string $value, \Closure $fail){
+    //             if(strtoupper($value) != $value){
+    //                 $fail("The field $attribute must be UPPERCASE");
+    //             }
+    //         }],
+    //         "password" => ["required", "min:6", "max:20", new RegistrationRule()]
+    //     ];
+
+    //     $validator = Validator::make($data, $rules);
+    //     self::assertNotNull($validator);
+
+    //     self::assertFalse($validator->passes());
+    //     self::assertTrue($validator->fails());
+
+    //     $message = $validator->getMessageBag();
+
+    //     Log::info($message->toJson(JSON_PRETTY_PRINT));
+    // }
+
+    // public function testValidatorRuleClasses()
+    // {
+    //     $data = [
+    //         "username" => "Eko",
+    //         "password" => "eko@pzn123.com"
+    //     ];
+
+    //     $rules = [
+    //         "username" => ["required", new In(["Eko", "Budi", "Joko"])],
+    //         "password" => ["required", Password::min(6)->letters()->numbers()->symbols()]
+    //     ];
+
+    //     $validator = Validator::make($data, $rules);
+    //     self::assertNotNull($validator);
+
+    //     self::assertTrue($validator->passes());
+    // }
+
+    // public function testNestedArray()
+    // {
+    //     $data = [
+    //         "name" => [
+    //             "first" => "Eko",
+    //             "last" => "Kurniawan"
+    //         ],
+    //         "address" => [
+    //             "street" => "Jalan. Mangga",
+    //             "city" => "Jakarta",
+    //             "country" => "Indonesia"
+    //         ]
+    //     ];
+
+    //     $rules = [
+    //         "name.first" => ["required", "max:100"],
+    //         "name.last" => ["max:100"],
+    //         "address.street" => ["max:200"],
+    //         "address.city" => ["required", "max:100"],
+    //         "address.country" => ["required", "max:100"],
+    //     ];
+
+    //     $validator = Validator::make($data, $rules);
+    //     self::assertTrue($validator->passes());
+
+    // }
+
+    // public function testNestedIndexedArray()
+    // {
+    //     $data = [
+    //         "name" => [
+    //             "first" => "Eko",
+    //             "last" => "Kurniawan"
+    //         ],
+    //         "address" => [
+    //             [
+    //                 "street" => "Jalan. Mangga",
+    //                 "city" => "Jakarta",
+    //                 "country" => "Indonesia"
+    //             ],
+    //             [
+    //                 "street" => "Jalan. Manggis",
+    //                 "city" => "Jakarta",
+    //                 "country" => "Indonesia"
+    //             ]
+    //         ]
+    //     ];
+
+    //     $rules = [
+    //         "name.first" => ["required", "max:100"],
+    //         "name.last" => ["max:100"],
+    //         "address.*.street" => ["max:200"],
+    //         "address.*.city" => ["required", "max:100"],
+    //         "address.*.country" => ["required", "max:100"],
+    //     ];
+
+    //     $validator = Validator::make($data, $rules);
+    //     self::assertTrue($validator->passes());
+
+    // }
 }
