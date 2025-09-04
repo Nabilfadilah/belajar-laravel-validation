2e038d17ea1ac15fb7dff5352239557c8e51c1dc
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-09-04 08:48:59 +0700

    Menjalankan validasi dan unit test

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -26,5 +26,31 @@ class ValidatorTest extends TestCase
         // lakukan validator
         $validator = Validator::make($data, $rules);
         self::assertNotNull($validator); // hasilnya 
+
+        // menjalankan validasi
+        self::assertTrue($validator->passes()); // passes(), akan mengembalikan true jika sukses, false jika gagal
+        self::assertFalse($validator->fails()); // fails(), akan mengembalikan true jika gagal, false jika sukses
+    }
+
+    public function testInValidator(): void
+    {
+        // variable data, yg isinya array  
+        $data = [
+            "username" => "",
+            "password" => "",
+        ];
+
+        $rules  = [
+            "username" => "required",
+            "password" => "required",
+        ];
+
+        // lakukan validator
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator); // hasilnya 
+
+        // menjalankan validasi
+        self::assertFalse($validator->passes()); // passes(), akan mengembalikan true jika sukses, false jika gagal
+        self::assertTrue($validator->fails()); // fails(), akan mengembalikan true jika gagal, false jika sukses
     }
 }
