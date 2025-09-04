30ba473b992d94a34784e20038ec643e6b78c492
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-09-04 11:07:35 +0700

    Alur Validation Error web dan unit test

---
+++ resources/views/form.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+
+        <form action="/form" method="POST">
+            @csrf
+            <label>Username : <input type="text" name="username"></label>
+            <label>Password : <input type="password" name="password"></label>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</body>
+
+</html>
